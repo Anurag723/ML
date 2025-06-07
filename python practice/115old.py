376cabea4cc451929c451537dd7566d4d613a00d
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-06-07 19:22:18 +0000

    cs231

---
+++ python practice/115old.py
@@ -0,0 +1,10 @@
+month = int(input("Enter the month"))
+age = int(input("Enter the age"))
+
+dbl = month*2
+ad = dbl + 5
+
+no = (ad*50)+age-365
+
+print("spl no is:",no)
+print("age:",age,"\nmonth:",month, "\nspl no:",no)
