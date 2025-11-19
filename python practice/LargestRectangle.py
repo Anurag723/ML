333fd62f0436cd7f6c0eb7491b5b1c8cb2e6382b
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-11-19 16:58:39 +0000

    done

--- python practice/LargestRectangle.py
+++ python practice/LargestRectangle.py
@@ -1,3 +1,17 @@
+def largestRectangleAreaII(self, heights):
+        stack = []  
+        heights.append(0)  
+        max_area = 0
+
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] > h:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
+
+        return max_area
+
 def largestRectangleArea(heights):
     stack = []
     max_area = 0
