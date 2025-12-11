cc76f1fe7612ab2498b3581cc1f6f4c4f852c122
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-12-11 18:11:21 +0000

    Pointer based approach

--- python practice/SimplifyPath.py
+++ python practice/SimplifyPath.py
@@ -14,6 +14,26 @@ class SimplifyPath:
                 stack.append(part)
 
         return "/" + "/".join(stack)
+    
+
+    # for that of pointer based approach   
+    def simplifyPath1(self, path: str) -> str:
+        parts = path.split("/")
+        result = [None] * len(parts)  # Pre-allocated list
+        i = 0  # acts like the top of the stack
+
+        for part in parts:
+            if part == "" or part == ".":
+                continue
+            elif part == "..":
+                if i > 0:
+                    i -= 1  # Move pointer back (delete last folder)
+            else:
+                result[i] = part  # Place folder at current pointer
+                i += 1             # Move pointer forward
+
+        return "/" + "/".join(result[:i])
+
 
 
 if __name__ == "__main__":
